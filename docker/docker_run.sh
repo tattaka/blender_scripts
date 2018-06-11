bcb0eae3e2e3a40a46b8c78b44958e5c7d3be1db
tattaka <taka_aki@me.com>
2018-06-11 13:54:20 +0900

    get image-dateset

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,3 +1,4 @@
+docker rm $1
 docker run -it --runtime=nvidia --net=host -e DISPLAY=$DISPLAY --name $1\
 	-e NVIDIA_DRIVER_CAPABILITIES=all -e NVIDIA_VISIBLE_DEVICES=all \
 	-v $HOME/blender/scripts:/root/blender:rw -v \
